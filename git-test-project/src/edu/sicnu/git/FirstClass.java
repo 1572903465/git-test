b62d7aa7eb223bb2c890634ab428fe4999b28f7e
admin <1572903465@qq.com>
2020-10-02 19:29:43 +0800

    idea里面第一次提交Java项目

---
+++ git-test-project/src/edu/sicnu/git/FirstClass.java
@@ -0,0 +1,11 @@
+package edu.sicnu.git;
+
+public class FirstClass {
+    public static void main(String[] args) {
+        System.out.println("hello git projiect");
+        System.out.println("hello git projiect");
+        System.out.println("hello git projiect");
+        System.out.println("hello git projiect");
+
+    }
+}
